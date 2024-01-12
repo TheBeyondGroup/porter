9e317627867870f1d0076f0b81f743bba3f7d31e
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2024-01-11 17:03:10 -0800

    feat: add protocol flag to pack install
    I want the ability to use SSH to download private git repos. Some people
    would rather use HTTPS and enter a password. Let's give people a choice!

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,2 +1,6 @@
 export const LOCAL_PORTER_DIRECTORY = '.porter'
 export const CARRY_FILE = 'carry.json'
+export const PROTOCOL_SSH = 'ssh'
+export const PROTOCOL_HTTPS = 'https'
+export const GIT_PROTOCOLS = [PROTOCOL_SSH, PROTOCOL_HTTPS]
+export const GIT_FOLDER = '.git'
